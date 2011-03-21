9c918d181306a7248fb8e466833b593f97f60724
Olivier Mengué <dolmen@cpan.org>
2011-03-21 18:48:44 +0000

    Import of DOLMEN/Win32-App-which-1.00 from CPAN.
    gitpan-cpan-distribution: Win32-App-which
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         DOLMEN/Win32-App-which-1.00.tar.gz
    gitpan-cpan-author:       DOLMEN
    gitpan-cpan-maturity:     released

--- bin/which.cmd
+++ bin/which.cmd
@@ -1,36 +1,101 @@
 @echo off
-:: Copyright: -© 2010 Olivier Mengu+©
-:: License: GPLv3
+:: Copyright © 2010-2011 Olivier Mengu‚
+::
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see L<http://www.gnu.org/licenses/>.
 
-if "%1"=="" exit /B 1
-if not "%~x1"=="" if exist "%1" echo %~f1& goto :EOF
 setlocal
-set F=%~nx1
-call :$PathFinder P "%F%"
-call :Found?
-for %%e in (%PATHEXT%) do (
-    call :$PathFinder P "%F%%%e"
-    call :Found?
+set SPEC=First
+if "%~1"=="-a" (
+    set SPEC=All
+    shift
 )
+if "%~1"=="--" shift
 
-if "%Found%"=="1" goto :OK
-echo %F% not found.>&2
-endlocal
+if "%~1"=="" exit /B 2
+
+:: Add the current directory to the PATH
+call :$AbsolutePath P .
+set "PATH=%P%;%PATH%"
+
+set Found=
+
+:: Shortcut for this special case that the shell knows how to handle
+if %SPEC%==First if not "%~x1"=="" goto :FirstExtBuiltin
+
+::Slow
+if not "%~x1"=="" call :VisitPATH :Visit%SPEC%Ext "%~1"
+if not "%~x1"=="" (
+    if not #%Found%==#1 call :VisitPATH :Visit%SPEC% "%~n1"
+) else (
+    call :VisitPATH :Visit%SPEC% "%~1"
+)
+:End
+endlocal & if #%Found%==#1 exit /B 0
+echo %~1 not found.>&2
 exit /B 1
 
-:Found?
-if "%P%"=="" goto :EOF
-if "%~f1"=="" call :Found?2 "%P%" & goto :EOF
-:Found?2
-if "%~x1"=="" goto :EOF
+:: Use builtin for this special case
+:FirstExtBuiltin
+set "Found=%~$PATH:1"
+if not "%Found%"=="" echo %Found%& set Found=1
+goto :End
+
+:: Test one filename
+:VisitFirstExt2
+if #%Found%==#1 goto :EOF
+:VisitFirstExt
+:VisitAllExt
+:VisitAllExt1
+:VisitAllExt2
+::echo %~1 ?
+if not exist "%~1" goto :EOF
+echo %~f1
 set Found=1
-echo.%P%
 goto :EOF
 
-:OK
-endlocal
-exit /B 0
-
-:$PathFinder
-set "%1=%~$PATH:2"
+:: Test all extensions for the %1 filename
+:VisitFirst
+:VisitAll
+:VisitAll1
+for %%e in (%PATHEXT%) do call :Visit%SPEC%Ext2 "%~1%%e"
+:VisitFirst1
+:VisitFirstExt1
 goto :EOF
+
+:$AbsolutePath
+set "%1=%~f2"
+goto :EOF
+
+:: VisitPATH
+::
+:: %1 callback name
+::
+: == VisitPATH ==
+set "VisitPATH_P=%PATH%"
+call :VisitPATH_Loop . %1 "%~2"
+goto :EOF
+
+:VisitPATH_Loop
+set "VisitPATH_Q=%PATH%"
+:: Find the first "." (%1) in the reduced %PATH% => first directory in %PATH%
+set "PATH=%VisitPATH_P%"& call %2%Found% "%~$PATH:1\%~3"
+:: Delete the first path
+set "PATH=%VisitPATH_Q:*;=%"
+if not "%PATH%"=="%VisitPATH_Q%" goto :VisitPATH_Loop
+
+set "PATH=%VisitPATH_P%"
+set VisitPATH_P=
+set VisitPATH_Q=
+goto :EOF
+
